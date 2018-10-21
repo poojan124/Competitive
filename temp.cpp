760a88e0cba7110266432d77079dffd8f16d19b1
poojan124 <poojan124@hotmail.com>
2018-10-21 10:00:29 -0400

    change

--- temp.cpp
+++ temp.cpp
@@ -1,29 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-long long int compute(bool ar[], long long int st, long long int en){
+int rev(int x,int k){
+  int rev = 0;
+  while(x){
+    rev<<=1;
+    rev |= x&1;
+    x>>=1;
+    k--;
+  }
+  return rev<<k;
+}
+int compute(bool ar[], int st, int en){
   bool x = ar[st], y = ar[st];
   if(en==st)
     return 0;
-  for(long long int i=st+1;i<=en;i++){
+  for(int i=st+1;i<=en;i++){
     x = x&ar[i];
-    y = y^ar[i];
+    y = y|ar[i];
   }
-  if(x==1 || y ==0)
+  if(x==1 || y==0)
     return 0;
-  long long int mid = st+(en-st)/2;
-  //cout<<mid<<endl;
+  int mid = st+(en-st)/2;
   return 2+compute(ar, st, mid)+compute(ar, mid+1, en);
 }
 int main(){
-  long long int n;
+  int n;
   cin>>n;
-  long long int x = 1<<n;
-  bool ar[x+1];
-  for(long long int i=1;i<=x;i++)
-    cin>>ar[i];
+  int x=1<<n;
+  bool ar[x];
+  assert(rev(x-1,n)==x-1);
+  for(int i=0;i<x;i++){
+    cin>>ar[rev(i)];
+  }
   //cout<<(ar[1]&ar[2]);
-  reverse(ar+1,ar+n);
-  cout<<compute(ar,1,x)+1;
+  cout<<compute(ar,0,x-1)+1;
   return 0;
 }
